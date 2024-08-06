32436308ad6b1027845090db6b807e9d43527b87
Fernando Soares <fernando_soares346@hotmail.com>
2024-08-06 22:14:10 +0100

    feat: :sparkles: created Country and City routes

---
+++ src/app/[country]/page.tsx
@@ -0,0 +1,143 @@
+import { notFound } from "next/navigation";
+import { Country } from "country-state-city";
+
+import { CountryRouteParams, RouteParams } from "@/types/globals";
+
+import { slugify } from "@/lib/utils";
+
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+
+function fetchCountryData(slug: string) {
+	const countries = Country.getAllCountries();
+	return countries.find((country) => slugify(country.name) === slug);
+}
+
+export function generateStaticParams() {
+	const countries = Country.getAllCountries();
+	return countries.map((country) => ({ country: slugify(country.name) }));
+}
+
+export default function CountryPage({ params }: RouteParams<CountryRouteParams>) {
+	const country = fetchCountryData(params.country);
+
+	if (!country) notFound();
+
+	return (
+		<Table>
+			<TableHeader>
+				<TableRow>
+					<TableHead>Customer</TableHead>
+					<TableHead className="hidden sm:table-cell">Type</TableHead>
+					<TableHead className="hidden sm:table-cell">Status</TableHead>
+					<TableHead className="hidden md:table-cell">Date</TableHead>
+					<TableHead className="text-right">Amount</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				<TableRow className="bg-accent">
+					<TableCell>
+						<div className="font-medium">Liam Johnson</div>
+						<div className="hidden text-sm text-muted-foreground md:inline">
+							liam@example.com
+						</div>
+					</TableCell>
+					<TableCell className="hidden sm:table-cell">Sale</TableCell>
+					<TableCell className="hidden sm:table-cell">Fulfilled</TableCell>
+					<TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+					<TableCell className="text-right">$250.00</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<div className="font-medium">Olivia Smith</div>
+						<div className="hidden text-sm text-muted-foreground md:inline">
+							olivia@example.com
+						</div>
+					</TableCell>
+					<TableCell className="hidden sm:table-cell">Refund</TableCell>
+					<TableCell className="hidden sm:table-cell">Declined</TableCell>
+					<TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+					<TableCell className="text-right">$150.00</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<div className="font-medium">Noah Williams</div>
+						<div className="hidden text-sm text-muted-foreground md:inline">
+							noah@example.com
+						</div>
+					</TableCell>
+					<TableCell className="hidden sm:table-cell">Subscription</TableCell>
+					<TableCell className="hidden sm:table-cell">Fulfilled</TableCell>
+					<TableCell className="hidden md:table-cell">2023-06-25</TableCell>
+					<TableCell className="text-right">$350.00</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<div className="font-medium">Emma Brown</div>
+						<div className="hidden text-sm text-muted-foreground md:inline">
+							emma@example.com
+						</div>
+					</TableCell>
+					<TableCell className="hidden sm:table-cell">Sale</TableCell>
+					<TableCell className="hidden sm:table-cell">Fulfilled</TableCell>
+					<TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+					<TableCell className="text-right">$450.00</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<div className="font-medium">Liam Johnson</div>
+						<div className="hidden text-sm text-muted-foreground md:inline">
+							liam@example.com
+						</div>
+					</TableCell>
+					<TableCell className="hidden sm:table-cell">Sale</TableCell>
+					<TableCell className="hidden sm:table-cell">Fulfilled</TableCell>
+					<TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+					<TableCell className="text-right">$250.00</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<div className="font-medium">Liam Johnson</div>
+						<div className="hidden text-sm text-muted-foreground md:inline">
+							liam@example.com
+						</div>
+					</TableCell>
+					<TableCell className="hidden sm:table-cell">Sale</TableCell>
+					<TableCell className="hidden sm:table-cell">Fulfilled</TableCell>
+					<TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+					<TableCell className="text-right">$250.00</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<div className="font-medium">Olivia Smith</div>
+						<div className="hidden text-sm text-muted-foreground md:inline">
+							olivia@example.com
+						</div>
+					</TableCell>
+					<TableCell className="hidden sm:table-cell">Refund</TableCell>
+					<TableCell className="hidden sm:table-cell">Declined</TableCell>
+					<TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+					<TableCell className="text-right">$150.00</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<div className="font-medium">Emma Brown</div>
+						<div className="hidden text-sm text-muted-foreground md:inline">
+							emma@example.com
+						</div>
+					</TableCell>
+					<TableCell className="hidden sm:table-cell">Sale</TableCell>
+					<TableCell className="hidden sm:table-cell">Fulfilled</TableCell>
+					<TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+					<TableCell className="text-right">$450.00</TableCell>
+				</TableRow>
+			</TableBody>
+		</Table>
+	);
+}
