d667ce7b6f1710ca1e35f2ce8590941570bcb5f8
Fernando Soares <fernando_soares346@hotmail.com>
2024-09-02 19:57:12 +0100

    feat: :sparkles: added error handling to translations

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,7 @@
 import { notFound } from "next/navigation";
+import { IntlErrorCode } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
+
 import { routing } from "./routing";
 
 export default getRequestConfig(async ({ locale }) => {
@@ -8,5 +10,17 @@ export default getRequestConfig(async ({ locale }) => {
 
 	return {
 		messages: (await import(`./dictionary/${locale}.json`)).default,
+		onError(error) {
+			console.log(error);
+		},
+		getMessageFallback({ namespace, key, error }) {
+			const path = [namespace, key].filter((part) => part != null).join(".");
+
+			if (error.code === IntlErrorCode.MISSING_MESSAGE) {
+				return path + " is not yet translated";
+			} else {
+				return "Dear developer, please fix this message: " + path;
+			}
+		},
 	};
 });
