eedbb73572a37d1f7d87cb874134f1763af6ea03
Fernando Soares <fernando_soares346@hotmail.com>
2024-09-05 19:25:57 +0100

    build: :wrench: added RestCountries img asset domains to next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "flagcdn.com",
+			},
+			{
+				protocol: "https",
+				hostname: "*.wikimedia.org",
+			},
+		],
+	},
+};
 
 export default withNextIntl(nextConfig);
