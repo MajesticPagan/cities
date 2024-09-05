730b5c811449ea754d5fe48ace877f19a2102b1d
Fernando Soares <fernando_soares346@hotmail.com>
2024-09-05 19:22:05 +0100

    feat: :sparkles: added fetchApi helper function and wrapper for RestCountries

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -6,3 +6,19 @@ export function fetchCountryBySlug(slug: string) {
 	const countries = Country.getAllCountries();
 	return countries.find((country) => slugify(country.name) === slug);
 }
+
+export async function fetchApi(url: string, endpoint: string) {
+	const response = await fetch(`${url}/${endpoint}`, {
+		next: { revalidate: 3600 },
+	});
+
+	if (!response.ok) {
+		throw new Error("Failed to fetch data. Please try again later.");
+	}
+
+	return response.json();
+}
+
+export async function fetchCountries(endpoint: string) {
+	return await fetchApi("https://restcountries.com/v3.1", endpoint);
+}
